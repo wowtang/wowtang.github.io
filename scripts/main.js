95f217b3096df527abdb2e06dd0491e29325be0c
wowtang <2468841720@qq.com>
2026-08-08 16:34:24 +0800

    Deployed by Gridea Pro: 2026-08-08 16:34:24

--- scripts/main.js
+++ scripts/main.js
@@ -650,18 +650,13 @@
       if (!modal) createModal();
       modal.querySelector('#link-out-url-value').textContent = url;
       modal.classList.add('active');
-      var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-      if (scrollbarWidth > 0) {
-        document.body.style.paddingRight = scrollbarWidth + 'px';
-      }
       document.body.style.overflow = 'hidden';
     }
-    
+
     function closeModal() {
       if (modal) {
         modal.classList.remove('active');
         document.body.style.overflow = '';
-        document.body.style.paddingRight = '';
         targetUrl = null;
       }
     }
